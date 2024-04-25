f5a1af7196f7831758fa21a46498401c09cd1935
Code Monad <osprey.detoxes_07@icloud.com>
2024-04-26 03:32:33 +0800

    cargo clippy

--- src/app.rs
+++ src/app.rs
@@ -439,9 +439,7 @@ fn current_encode_method(ui: &mut egui::Ui, app: &mut TemplateApp) {
         if total < 1.0 {
             ui.label("|");
             ui.label(
-                egui::RichText::new(format!(
-                    "WARN: Total payment cannot be less than 1 Shannon!!"
-                ))
+                egui::RichText::new("WARN: Total payment cannot be less than 1 Shannon!!".to_string())
                 .color(egui::Color32::RED),
             );
         }
